1a819ec399847508b7294e0f4f98fe1c9954be32
KentGreco <madsvillainroblox@gmail.com>
2026-04-05 00:05:15 -0400

    update map.js

--- js/map.js
+++ js/map.js
@@ -1,2 +1,15 @@
 // DAYMAP - Map module
 // Handles Leaflet map initialization and controls
+
+let map;
+
+function initMap(){
+    map = L.map('map').setView([39.4799, -76.6408], 12);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetmap contributors',
+        maxZoom: 19
+    }).addTo(map);
+
+    //Test pin:
+    L.marker([39.4799, -76.6408]).addTo(map).bindPopup('<b>DAYMAP</b><br>Cockeysville, MD').openPopup();
+}
