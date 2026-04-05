de4dcc74fc91f842ff2099b6f4d0ecd3538bff98
KentGreco <madsvillainroblox@gmail.com>
2026-04-04 23:34:04 -0400

    update app.js

--- js/app.js
+++ js/app.js
@@ -1,2 +1,11 @@
 // DAYMAP - Main app controller
 // Source for all app logic
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Display today date in header
+    const dateEl = document.getElementById('current-date');
+    const today = new Date();
+    dateEl.textContent = today.toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' });
+
+    initMap();
+});
